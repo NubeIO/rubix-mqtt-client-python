72f6c210448ba7924557c9c670d3cd4f628c2e61
enjuthulung <enjuthulung@gmail.com>
2021-08-24 12:38:36 +0545

    Add write schedule value

--- rubix/rubix_schedule.py
+++ rubix/rubix_schedule.py
@@ -1,5 +1,7 @@
+import json
 from typing import List
 
+from rubix.mqtt_client.mqtt_client import MqttClient
 from rubix.store import Store, Response
 
 
@@ -16,3 +18,19 @@ def get_schedules_by_device_id(device_id: str) -> List:
     if response:
         return response.schedules
     return []
+
+
+def write_schedule_value_by_uuid(device_id: str, uuid: str, payload: dict):
+    device: Response = Store().get_by_device_id(device_id)
+    if not device:
+        return
+    topic: str = f'{device.client_id}/{device.site_id}/{device.device_id}/rubix/points/listen/schedules/uuid/{uuid}'
+    MqttClient().publish_value(topic, json.dumps(payload))
+
+
+def write_schedule_value_by_name(device_id: str, name: str, payload: dict):
+    device: Response = Store().get_by_device_id(device_id)
+    if not device:
+        return
+    topic: str = f'{device.client_id}/{device.site_id}/{device.device_id}/rubix/points/listen/schedules/name/{name}'
+    MqttClient().publish_value(topic, json.dumps(payload))
